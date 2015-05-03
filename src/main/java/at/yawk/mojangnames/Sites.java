4cd4281bdabda6db1d19c853866e1f82b0c1fe2a
Jonas Konrad <me@yawk.at>
2015-05-03 13:10:57 +0200

    Allow no-dash urls and fix api url for name history

--- src/main/java/at/yawk/mojangnames/Sites.java
+++ src/main/java/at/yawk/mojangnames/Sites.java
@@ -5,6 +5,7 @@ import at.yawk.mojangapi.NameHistory;
 import at.yawk.mojangapi.Profile;
 import java.io.IOException;
 import java.util.UUID;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,12 +19,24 @@ import org.springframework.web.servlet.ModelAndView;
 public class Sites {
     @Autowired EndpointProvider endpointProvider;
 
+    @RequestMapping("/{uuid:[0-9a-f]{32}}")
+    void noDashesUUID(HttpServletResponse response, @PathVariable String uuid) throws IOException {
+        String withDashes =
+                uuid.substring(0, 8) + '-' +
+                uuid.substring(8, 12) + '-' +
+                uuid.substring(12, 16) + '-' +
+                uuid.substring(16, 20) + '-' +
+                uuid.substring(20, 32);
+        response.sendRedirect(withDashes);
+    }
+
     @RequestMapping("/{uuid:[0-9a-f]{8}-(?:[0-9a-f]{4}-){3}[0-9a-f]{12}}")
     ModelAndView history(@PathVariable UUID uuid) throws IOException, InterruptedException {
         NameHistory history = endpointProvider.nameHistory().call(uuid);
         return new ModelAndView("name_history")
                 .addObject("history", history)
-                .addObject("uuid", uuid);
+                .addObject("uuid", uuid)
+                .addObject("uuid_no_dashes", uuid.toString().replace("-", ""));
     }
 
     @RequestMapping("/{name:\\w+}")
